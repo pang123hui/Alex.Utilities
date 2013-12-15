e2b0e363c5c6e81562c40e9ce105d892f9f5609b
pang123hui <dps360@sina.com>
2013-12-15 19:44:05 +0800

    Add a file call StringHelper.cs
    1. Add a file call StringHelper.cs.
    2. Add some function in StringHelper.cs file. Those function are operate
    of the String.

---
+++ Alex.Utilities/字符串/StringHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Alex.Utilities
+{
+    /// <summary>
+    /// 字符串操作类
+    /// 
+    /// Time: 2013/12/15
+    /// Versions: 00.01
+    /// 
+    /// 1. public static bool isNumber( string str ) 检查一个字符串是否是纯数字构成的，一般用于查询字符串参数的有效性验证
+    /// 2. public static bool isDecimal( string str ) 检查一个字符串是否是浮点数构成的，一般用于查询字符串参数的有效性验证
+    /// </summary>
+    public class StringHelper
+    {
+    #region  字符串组成的验证
+        /// <summary>
+        /// 检查一个字符串是否是纯数字构成的，一般用于查询字符串参数的有效性验证
+        /// </summary>
+        /// <param name="str">需验证的字符串</param>
+        /// <returns>是否合法的bool值</returns>
+        public static bool isNumber( string str )
+        {
+            try
+            {
+                int data = Convert.ToInt32(str);
+                return true;
+            }
+            catch
+            {
+                return false;
+            	
+            }
+        }
+        /// <summary>
+        /// 检查一个字符串是否是浮点数构成的，一般用于查询字符串参数的有效性验证
+        /// </summary>
+        /// <param name="str">需验证的字符串</param>
+        /// <returns>是否合法的bool值</returns>
+        public static bool isDecimal( string str )
+        {
+            try
+            {
+                Decimal data = Convert.ToDecimal(str);
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+        }
+    #endregion
+    }
+}
